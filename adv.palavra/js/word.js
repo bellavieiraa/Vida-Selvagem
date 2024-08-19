006d75ed8a67776307659724b044f0b5a83b38b7
bellavieiraa Vieira <isabella.vieira@eaportal.org>
2024-08-19 19:55:35 +0000

    projeto finalizado

---
+++ adv.palavra/js/word.js
@@ -0,0 +1,23 @@
+const wordList = [
+    {
+        word: "planta",
+        hint: "exemplo de produtores"
+    },
+    {
+        word: "grilo",
+        hint: "exemplo de consumidor primário"
+    },
+    {
+        word: "sapo",
+        hint: "exemplo de consumidor secundário"
+    },
+    {
+        word: "cobra",
+        hint: "exemplo de consumidor terciário"
+    },
+    {
+        word: "fungos",
+        hint: "exemplo de decompositores"
+    },
+    
+]
